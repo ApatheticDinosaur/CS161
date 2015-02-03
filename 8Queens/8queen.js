bbd44180323da9a50a8ee2fe20dc767b94a9df90
De.Thorpe <cs.temp.mail@gmail.com>
2015-02-02 19:54:49 -0800

    Added switch statement function for board size.

--- 8Queens/8queen.js
+++ 8Queens/8queen.js
@@ -1,1 +1,22 @@
-javascript functions here
+//javascript functions here
+
+function changeSize(selection){
+	var i;
+	switch(selection.selectedIndex){
+		case 0:
+			i = 0;
+			break;
+		case 1:
+			i = 0;
+			break;
+		case 2:
+			i = 0;
+			break;
+		case 3:
+			i = 0;
+			break;
+		case 4:
+			i = 0;
+			break;
+	}
+}
